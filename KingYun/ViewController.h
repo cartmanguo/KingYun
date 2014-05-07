da7a21add4b6f72b956fa0c9bf605ba6e237cd02
cartmanguo <nggch1987@163.com>
2014-05-07 23:11:08 +0800

    Initial Commit

---
+++ KingYun/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KingYun
+//
+//  Created by Cartman on 14-5-7.
+//  Copyright (c) 2014年 Cartman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KingYunTests/KingYunTests.m
@@ -0,0 +1,34 @@
+//
+//  KingYunTests.m
+//  KingYunTests
+//
+//  Created by Cartman on 14-5-7.
+//  Copyright (c) 2014年 Cartman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KingYunTests : XCTestCase
+
+@end
+
+@implementation KingYunTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
